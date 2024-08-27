74df28e314d018ab91d9948b2ae7f621012a3e5d
akash <kumar111aakash.in@gmail.com>
2024-08-27 14:28:37 +0530

    Initial commit: Setup Django project with basic authentication

---
+++ core/views.py
@@ -0,0 +1,38 @@
+#from django.shortcuts import render
+
+# core/views.py
+
+from django.http import HttpResponse
+
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from .serializers import SignupSerializer, LoginSerializer
+
+
+def home(request):
+    return HttpResponse("Welcome to the Social Network API")
+
+class SignupView(APIView):
+    def post(self, request):
+        serializer = SignupSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = authenticate(
+                username=serializer.validated_data['username'],
+                password=serializer.validated_data['password']
+            )
+            if user is not None:
+                return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
+            return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
